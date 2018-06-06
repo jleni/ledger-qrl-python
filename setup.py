1879ce04e39422054b7b59e6b7f3eb96e603c605
Juan Leni <lenijuan@gmail.com>
2018-06-06 20:00:45 +0200

    Indicate minimum version that is supported

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pyledgerqrl',
-    version='0.3',
+    version='0.4.0',
     description='python module for Ledger Nano S',
     url='https://github.com/theQRL/ledger-qrl',
     author='The QRL Team',
