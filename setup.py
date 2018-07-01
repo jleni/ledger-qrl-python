111165a9ba3a99d8c2fea960d24a5260bf7b7973
Juan Leni <lenijuan@gmail.com>
2018-07-01 16:05:21 +0200

    upgrading ledgerblue requirement

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='pyledgerqrl',
-    version='0.4.0',
-    description='python module for Ledger Nano S',
-    url='https://github.com/theQRL/ledger-qrl',
-    author='The QRL Team',
-    author_email='info@theqrl.org',
-    license='MIT',
-    packages=['pyledgerqrl'],
-    install_requires=['ledgerblue'],
-    zip_safe=False)
+      version='0.5.0',
+      description='python module for Ledger Nano S',
+      url='https://github.com/theQRL/ledger-qrl',
+      author='The QRL Team',
+      author_email='info@theqrl.org',
+      license='MIT',
+      packages=['pyledgerqrl'],
+      install_requires=['ledgerblue>0.1.18'],
+      zip_safe=False)
